c4122ca580113ac658149ba914fccb6e387404ec
0Lolita <92529478+0Lolita@users.noreply.github.com>
2025-05-20 13:39:09 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,21 +2,21 @@
 git clone --depth 1 https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
-git clone --depth 1 https://github.com/sirpdboy/luci-app-partexp package/otherapp/luci-app-partexp
-git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice package/otherapp/luci-app-poweroffdevice
-git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard package/otherapp/luci-app-netwizard
-git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset package/otherapp/luci-app-autotimeset
-git clone --depth 1 https://github.com/sirpdboy/netspeedtest package/otherapp/netspeedtest
-git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced package/otherapp/luci-app-advanced
-git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus.git package/otherapp/luci-app-advancedplus
-git clone --depth 1 https://github.com/AlexZhuo/luci-app-bandwidthd package/otherapp/luci-app-bandwidthd
-git clone --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon package/otherapp/luci-app-wrtbwmon
-git clone --depth 1 https://github.com/ilxp/luci-app-ikoolproxy package/otherapp/luci-app-ikoolproxy
-git clone --depth 1 https://github.com/frainzy1477/luci-app-clash package/otherapp/luci-app-clash
-git clone --depth 1 https://github.com/OpenWrt-Actions/luci-app-vssr package/otherapp/luci-app-vssr
+git clone --depth 1 https://github.com/sirpdboy/luci-app-partexp
+git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
+git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard
+git clone --depth 1 https://github.com/sirpdboy/luci-app-autotimeset
+git clone --depth 1 https://github.com/sirpdboy/netspeedtest
+git clone --depth 1 https://github.com/sirpdboy/luci-app-advanced
+git clone --depth 1 https://github.com/sirpdboy/luci-app-advancedplus.git
+git clone --depth 1 https://github.com/AlexZhuo/luci-app-bandwidthd
+git clone --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon
+git clone --depth 1 https://github.com/ilxp/luci-app-ikoolproxy
+git clone --depth 1 https://github.com/frainzy1477/luci-app-clash
+git clone --depth 1 https://github.com/OpenWrt-Actions/luci-app-vssr
 git clone --depth 1 https://github.com/lucikap/luci-app-nettask.git package/luci-app-nettask
 git clone --depth 1 https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
-git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/otherapp/luci-theme-neobird
-git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
-git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/otherapp/unblockneteasemusic
-git clone --depth 1 https://github.com/vernesong/OpenClash.git package/otherapp/luci-app-openclash
+git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash

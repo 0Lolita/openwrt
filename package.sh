0c485d7fe662a4b4da60cc82f76829c3583f09b0
0Lolita <92529478+0Lolita@users.noreply.github.com>
2025-05-21 15:44:10 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,6 +2,11 @@
 git clone --depth 1 https://github.com/bigbugcc/OpenwrtApp package/otherapp/OpenwrtApp
 git clone --depth 1 https://github.com/destan19/OpenAppFilter package/otherapp/OpenAppFilter
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot package/otherapp/luci-app-pushbot
+git clone --depth 1 https://github.com/lucikap/luci-app-nettask.git package/luci-app-nettask
+git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 git clone --depth 1 https://github.com/sirpdboy/luci-app-partexp
 git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice
 git clone --depth 1 https://github.com/sirpdboy/luci-app-netwizard
@@ -14,8 +19,22 @@ git clone --depth 1 https://github.com/brvphoenix/luci-app-wrtbwmon
 git clone --depth 1 https://github.com/ilxp/luci-app-ikoolproxy
 git clone --depth 1 https://github.com/frainzy1477/luci-app-clash
 git clone --depth 1 https://github.com/OpenWrt-Actions/luci-app-vssr
-git clone --depth 1 https://github.com/lucikap/luci-app-nettask.git package/luci-app-nettask
-git clone --depth 1 https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
-git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
-git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+git clone --depth 1 https://github.com/zxl78585/luci-app-autoreboot.git package/luci-app-autoreboot
+git clone --depth 1 https://github.com/gngpp/luci-app-design-config.git package/luci-app-design-config
+git clone --depth 1 https://github.com/AlexZhuo/luci-app-haproxy-tcp package/luci-app-haproxy-tcp
+git clone --depth 1 https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+git clone --depth 1 https://github.com/lisaac/luci-app-diskman.git
+git clone --depth 1 https://github.com/kenzok78/luci-app-guest-wifi.git
+git clone --depth 1 https://github.com/AoThen/luci-app-ramfree.git
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-syncdial.git
+git clone --depth 1 https://github.com/mchome/openwrt-vlmcsd.git
+git clone --depth 1 https://github.com/mchome/luci-app-vlmcsd.git
+git clone --depth 1 https://github.com/animefansxj/luci-app-vsftpd.git
+
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+
+
+https://github.com/0Lolita/luci-app-smartdns
+https://github.com/0Lolita/openwrt-smartdns
